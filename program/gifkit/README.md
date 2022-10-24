0f3e8d5f4df2001cafcb072779b363192a1ba976
Paulo Bordignon <p.h_@outlook.com>
2022-10-24 00:06:23 -0300

    feat(program): added gifkit program

---
+++ program/gifkit/README.md
@@ -0,0 +1,5 @@
+<p align="center">
+Program Solana 
+</p>
+
+https://explorer.solana.com/address/AxBKG2ku3PqVYfQp9rZBU4tuNDLjZ7dxLxJqC8MndTe3?cluster=devnet
--- program/gifkit/tests/gifkit.js
+++ program/gifkit/tests/gifkit.js
@@ -1,13 +1,54 @@
 const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3;
 
-describe("gifkit", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+const main = async () => {
+  console.log("🚀 Iniciando testes...");
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const program = anchor.workspace.Gifkit;
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.Gifkit;
+  const baseAccount = anchor.web3.Keypair.generate();
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
   });
-});
+  console.log("📝 Sua assinatura de transação", tx);
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("👀 GIF Count", account.totalGifs.toString());
+
+  // Você precisará agora passar um link do GIF para a função! Você também precisará passar o usuário que está enviando o GIF!
+  await program.rpc.addGif(
+    "https://media2.giphy.com/media/rzCb3XwgyZLI58aWFD/giphy.gif?cid=ecf05e47idk1vnlwsjzerlods34ua5ew3cnxa6hxhyyn6ynu&rid=giphy.gif&ct=g",
+    {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+      },
+    }
+  );
+
+  // Chama a conta
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("👀 GIF Count", account.totalGifs.toString());
+
+  // Acessa o gif_list na conta
+  console.log("👀 GIF List", account.gifList);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
